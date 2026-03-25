be9c119bd6ef1735447ad3577fd949301b835bd9
vshedgeusa-eng <vshedge.usa@gmail.com>
2026-03-25 12:31:03 -0400

    Update streamlit_app.py
    Used pandas library instead of dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -20,7 +21,9 @@ st.write('The name on your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
