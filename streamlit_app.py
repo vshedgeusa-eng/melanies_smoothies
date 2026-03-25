df6dcae569aab0a37e4689704fc42bebac137aa1
vshedgeusa-eng <vshedge.usa@gmail.com>
2026-03-25 12:12:41 -0400

    Update streamlit_app.py
    Correction for fruit_choosen typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ if ingredients_list:
 
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)  
+        st.subheader(fruit_choosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
